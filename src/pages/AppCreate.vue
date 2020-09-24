9dd1c050ba7c755525335d2dd91bda2b0709cbe0
igrekus <upload234@mail.ru>
2020-09-24 19:25:43 +0300

    FIX: add account menu to the app create form

--- src/pages/AppCreate.vue
+++ src/pages/AppCreate.vue
@@ -12,7 +12,34 @@
       <div class="col-auto text-h5" style="max-width: available;"/>
       <div class="col-2">
         <q-btn round>
-          <q-icon name="face" style="font-size: 3rem;"/>
+          <q-icon name="account_circle" style="font-size: 3rem;"/>
+          <q-menu>
+            <div class="row no-wrap q-pa-md">
+              <div class="column">
+                <div class="text-h6 q-mb-md">Найстройки</div>
+                <q-toggle v-model="acc1" label="Сохранять историю"/>
+                <q-toggle v-model="acc2" label="Кешировать данные"/>
+              </div>
+
+              <q-separator vertical inset class="q-mx-lg"/>
+
+              <div class="column items-center">
+                <q-avatar size="72px">
+                  <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+                </q-avatar>
+
+                <div class="text-subtitle1 q-mt-md q-mb-xs">Иванов Иван</div>
+
+                <q-btn
+                        color="primary"
+                        label="Выйти"
+                        push
+                        size="sm"
+                        v-close-popup
+                />
+              </div>
+            </div>
+          </q-menu>
         </q-btn>
       </div>
     </div>
